f161f063b5fb2fc6cecdd343250c298fd3ece492
Christopher Gilbert <chris.gilbert@LASP.colorado.edu>
2019-11-20 17:46:51 -0700

    Initial commit after CMS Cookiecutter creation, version 1.1

---
+++ sunback/main.py
@@ -0,0 +1,34 @@
+"""
+main.py
+A program that downloads the most current images of the sun from the SDO satellite, then finds the most likely temperature in each pixel. Then it sets each of the images to the desktop background in series. 
+
+Handles the primary functions
+"""
+
+
+def canvas(with_attribution=True):
+    """
+    Placeholder function to show example docstring (NumPy format)
+
+    Replace this function and doc string for your own project
+
+    Parameters
+    ----------
+    with_attribution : bool, Optional, default: True
+        Set whether or not to display who the quote is from
+
+    Returns
+    -------
+    quote : str
+        Compiled string including quote and optional attribution
+    """
+
+    quote = "The code is but a canvas to our imagination."
+    if with_attribution:
+        quote += "\n\t- Adapted from Henry David Thoreau"
+    return quote
+
+
+if __name__ == "__main__":
+    # Do something if this file is invoked on its own
+    print(canvas())
