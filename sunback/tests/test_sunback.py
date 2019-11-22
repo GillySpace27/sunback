c646df62245eddbc6630894b2da840fa39d129fb
chgi7364 <chgi7364@colorado.edu>
2019-11-21 19:51:49 -0700

    New Test

--- sunback/tests/test_sunback.py
+++ sunback/tests/test_sunback.py
@@ -10,3 +10,11 @@ import sys
 def test_sunback_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "sunback" in sys.modules
+
+
+def test_download_works():
+    """Try to run the download process"""
+    web_path="https://sdo.gsfc.nasa.gov/assets/img/latest/latest_2048_{}}.jpg"
+    loc_path="test.jpg"
+    assert not sunback.download_image(web_path, loc_path, '193')
+
