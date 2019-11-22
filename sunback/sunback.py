f936bc78f17e63a5063aea12a2b8bf25794c3abd
chgi7364 <chgi7364@colorado.edu>
2019-11-21 17:06:17 -0700

    B

--- sunback/sunback.py
+++ sunback/sunback.py
@@ -33,7 +33,7 @@ sys.stderr = open("errlog.txt", "w")
 
 def download_image(web_path, local_path, wave):
     """
-    Download an image and save it to file
+    Download an image and save it to file asdf asdf
 
     Go to the internet and download an image
 
