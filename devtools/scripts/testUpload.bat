99992cb484a8e51f7278daa345ea897cd9f29dbb
chgi7364 <chgi7364@colorado.edu>
2019-11-21 18:56:49 -0700

    BatchScript

---
+++ devtools/scripts/testUpload.bat
@@ -0,0 +1,4 @@
+git commit -am "%2"
+git tag -a "%1" -m "%1 : %2"
+::python setup.py sdist bdist_wheel
+::python -m twine upload dist/* -r testpypi
--- devtools/scripts/testUpload.sh
+++
@@ -1,4 +0,0 @@
-git commit -am "$2"
-git tag -a "$1" -m "$1 : $2"
-python setup.py sdist bdist_wheel
-python -m twine upload dist/* -r testpypi
