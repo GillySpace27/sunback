2a5e35221b2369642d89e57fdec74a934700b02c
chgi7364 <chgi7364@colorado.edu>
2020-02-07 14:42:59 -0700

    Moved Save Directory

--- devtools/scripts/buildDistribution.bat
+++ devtools/scripts/buildDistribution.bat
@@ -2,4 +2,4 @@ git commit -am %2
 git tag -a "%1" -m "%1 : ""%2""
 MOVE dist\* dist_old\
 python setup.py sdist bdist_wheel
-for /d /r . %d in (Sunback-*) do @if exist "%d" rd /s/q "%d"
+for /d /r . %%d in (Sunback-*) do @if exist "%%d" rd /s/q "%%d"
