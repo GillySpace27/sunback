873c267f38f6553f824811997e36a7749ad1aac3
gillyspace27 <gilly@swri.org>
2023-08-29 19:16:07 -0600

    Added Lingon Support

---
+++ run_server_lingon.py
@@ -0,0 +1,1 @@
+/Users/cgilbert/opt/anaconda3/envs/sunback_env/bin/python /Users/cgilbert/PycharmProjects/sunback/src/run/run_server_lingon.py
--- run_server_lingon.zsh
+++ run_server_lingon.zsh
@@ -1,5 +1,3 @@
 #!/bin/zsh
 
-source activate /Users/cgilbert/opt/anaconda3/envs/sunback_env
-
-python /Users/cgilbert/PycharmProjects/sunback/src/run/run_server_lingon.py
+/Users/cgilbert/opt/anaconda3/envs/sunback_env/bin/python /Users/cgilbert/PycharmProjects/sunback/src/run/run_server_lingon.py
